091142c58ed62e3b39ff89837938bc0019f8d1be
tremuradaniel <tremuradaniel@gmail.com>
2020-05-16 14:29:52 +0300

    cleaned up default code + changed tab icon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './storage/fonts/OpenSans-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
