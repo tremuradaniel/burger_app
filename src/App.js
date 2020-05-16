a0c511dfc3ceb638db57c4e094287166ef474ec4
tremuradaniel <tremuradaniel@gmail.com>
2020-05-16 15:59:13 +0300

    added burger builder container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import Layout from '../src/components/Layout/Layout';
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render () {
     return (
       <div>
         <Layout>
-          <h1>Test</h1>
+          <h1>Burger Builder</h1>
+          <BurgerBuilder></BurgerBuilder>
         </Layout>
       </div>
     )
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <Auxiliary>
+                <div>Graphical representation of the burger</div>
+                <div>Burger controler</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
