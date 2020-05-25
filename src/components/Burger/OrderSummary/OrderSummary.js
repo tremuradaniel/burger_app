361dc38c9d3d8537d4518944b5dd580c5c8a136d
danieltremura <daniel.tremura@tarainteractive.com>
2020-05-25 08:47:40 +0300

    custom button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -22,6 +22,8 @@ const orderSummary = (props) => {
                 {ingredientsSummary}
             </ul>
             <p>Continue to checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Auxiliary>
     );
 };
