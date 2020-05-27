22cdb1b656cbee9fb53a46f305a058b77382aac5
tremuradaniel <tremuradaniel@gmail.com>
2020-05-27 07:57:24 +0300

    logo and initial  sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxiliary>
         <Toolbar />
+        <SideDrawer/>
         <div>
             Toolbar, Sidebar, Backdrop
         </div>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+import classes from './SideDrawer.module.css';
+
+// import NavigationItem from './NavigationItem/NavigationItem';
+
+const sideDrawer = (props) => {
+    
+    return (
+        <div className={classes.sideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
