5aa115f6d9a462c9fbf0936c9b6c2febab9401b4
tremuradaniel <tremuradaniel@gmail.com>
2020-05-31 10:51:31 +0300

    adding axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from '../../axios-orders';
 
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
@@ -86,7 +87,22 @@ class BurgerBuilder extends Component {
     }
     
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Daniel T.',
+                address: {
+                    street: 'Test',
+                    zipCode: '4554654'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log('response', response))
+            .catch(error => console.log('error', error));
     }
 
     render () {
