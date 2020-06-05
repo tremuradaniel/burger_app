eab2d5139b3a31880f7d9ff768223b2ae907c61f
tremuradaniel <tremuradaniel@gmail.com>
2020-06-05 08:05:26 +0300

    added checkout container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Layout from '../src/hoc/Layout/Layout';
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render () {
@@ -10,6 +11,7 @@ class App extends Component {
         <Layout>
           <h1>Burger Builder</h1>
           <BurgerBuilder></BurgerBuilder>
+          <Checkout />
         </Layout>
       </div>
     )
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary';
+
+class Checkout extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
+    render () {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}>
+
+                </CheckoutSummary>
+            </div>
+        )
+    }
+}
+
+export default Checkout;
