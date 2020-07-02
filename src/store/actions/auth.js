6a45fc96bda231a99a0634809fc1405752534095
Daniel Tremura <tremuradaniel@gmail.com>
2020-07-02 21:32:12 +0300

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,6 +21,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -37,6 +51,7 @@ export const auth = (email, password, isSignup) => {
             .then(response => {
                 if (process.env.REACT_APP_ENV === 'dev') { console.log(response); }
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err => {
                 if (process.env.REACT_APP_ENV === 'dev') { console.log(err.response.data.error.message); }
